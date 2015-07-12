9e973d7bcbde4e1b11e69073bc3f2414bf20c31d
Aliyu Abdullahi <aliyuabdullahi@Andelas-MacBook-Pro-3.local>
2015-07-12 14:46:12 +0100

    add modal for form

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('Momofuku', ['ngMaterial', 'firebase']);
+var app = angular.module('Momofuku', ['ngMaterial', 'firebase','lumx']);
 app.config(function($mdThemingProvider) {
   $mdThemingProvider.theme('default')
     .primaryPalette('orange')
--- js/controllers/form-controller.js
+++ js/controllers/form-controller.js
@@ -1,13 +1,49 @@
-app.controller("FormController", ['$scope','$firebaseArray', '$firebaseObject', function($scope, $firebaseObject,$firebaseArray,$mdDialog) {
+
+app.controller("FormController", ['$scope','LxDialogService','LxNotificationService','$mdDialog','$firebaseArray', '$firebaseObject', 
+  function($scope,LxDialogService, $firebaseObject,$firebaseArray,$mdDialog,LxNotificationService) {
   var ref = new Firebase("https://momofuku.firebaseio.com");
-  $scope.newMessageText = "Yes its true";
-  $scope.showform = false;
-  $scope.messages = $firebaseArray(ref);
+   $scope.cities =['New York','Toronto','Sidney'];
+  $scope.showMealForm = false;
+  $scope.venues = [];
+
+  $scope.showForm = false;
    $scope.addMessage = function() {
     ref.child('cities').push({
       text: $scope.newMessageText
     });
   };
-  $scope.cities =['New York','Toronto','Sidney'];
+  $scope.hideMealForm = function(index){
+    $scope.showMealForm = false;
+  };
+  $scope.showMeal = function($index){
+    $scope.showMealForm = true;
+  };
+  $scope.showDialog = function(dialogId){
+    LxDialogService.open(dialogId);
+    $scope.showStateName = true;
+  };
+  $scope.save = function(cityName){
+    console.log(cityName);
+    $scope.cities.push(cityName);
+    console.log($scope.cities);
+    
+  };
+ 
+   $scope.showCityDetailsDialog = function(dialogId){
+    LxDialogService.open(dialogId);
+  };
+  $scope.addToVenue = function(venue){
+    $scope.venues.push(venue);
+    venue = '';
+  };
+
+
+
+
+
+
 
 }]);
+
+
+
