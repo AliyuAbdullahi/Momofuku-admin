dbcdfb5661782e886f65ba3655ba1826ac9b2765
Aliyu Abdullahi <aliyuabdullahi@Andelas-MacBook-Pro-3.local>
2015-07-14 00:48:08 +0100

    add show city dialog

--- js/controllers/form-controller.js
+++ js/controllers/form-controller.js
@@ -1,48 +1,85 @@
 
-app.controller("FormController", ['$scope','LxDialogService','LxNotificationService','$mdDialog','$firebaseArray', '$firebaseObject', 
-  function($scope,LxDialogService, $firebaseObject,$firebaseArray,$mdDialog,LxNotificationService) {
-  var ref = new Firebase("https://momofuku.firebaseio.com");
-   $scope.cities =['New York','Toronto','Sidney'];
-  $scope.showMealForm = false;
-  $scope.venues = [];
+app.controller("FormController", ['$scope','Cities','LxDialogService','LxNotificationService','$mdDialog','$firebaseArray', '$firebaseObject', 
+  function($scope,Cities,LxDialogService,LxNotificationService,$mdDialog,$firebaseArray,$firebaseObject) {
+   $scope.cities = Cities.all();
+   window.cities = $scope.cities;
 
-  $scope.showForm = false;
-   $scope.addMessage = function() {
-    ref.child('cities').push({
-      text: $scope.newMessageText
+
+    $scope.showProjectDialog = function(ev) { 
+      $mdDialog.show({
+        controller: ['$scope', '$mdDialog', function($scope, $mdDialog) {
+          $scope.restaurants = Cities.all();
+          console.log($scope.restaurants);
+          var self = $scope;
+          self.city = null;
+          $scope.submitted = false;
+          $scope.save = function(isValid){
+         
+            if(isValid){
+               Cities.create($scope.city, function(err) {
+                if(err) {
+                  console.log('An error occurred');
+                }
+                else {
+                  console.log($scope.city.name + ' city created!');
+                }
+                $mdDialog.hide();
+              });
+            }
+          };
+          $scope.close = function() {
+            $mdDialog.hide();
+          };
+        }],
+        templateUrl: 'views/dialog.html',
+        targetEvent: ev
+      });
+    };
+
+   $scope.showCityDialog = function(ev,cityId,city) { 
+    $mdDialog.show({
+      controller: ['$scope', '$mdDialog', function($scope, $mdDialog) {
+
+        console.log(city);
+        var self = $scope;
+
+        
+        $scope.restaurant = Cities.find(cityId,function(){
+          
+        });
+        
+        self.city = city;
+
+        $scope.submitted = false;
+        $scope.save = function(isValid){
+          var data ={
+            name: isValid
+          };
+          console.log(cityId.toString());
+          self.city.restaurant.name = isValid;
+          Cities.ref.child(cityId).child('restaurants').push(data);
+          $mdDialog.hide();
+          // if(isValid){
+          //    Cities.update(self.city,cityId, function(err) {
+          //         if(err) {
+          //           console.log('An error occurred');
+          //         }
+          //         else {
+
+          //           console.log(isValid + ' city created!');
+          //         }
+          //         
+          //       });
+          // }
+        };
+        $scope.close = function() {
+          $mdDialog.hide();
+        };
+      }],
+      templateUrl: 'views/city-dialog.html',
+      targetEvent: ev
     });
   };
-  $scope.hideMealForm = function(index){
-    $scope.showMealForm = false;
-  };
-  $scope.showMeal = function($index){
-    $scope.showMealForm = true;
-  };
-  $scope.showDialog = function(dialogId){
-    LxDialogService.open(dialogId);
-    $scope.showStateName = true;
-  };
-  $scope.save = function(cityName){
-    console.log(cityName);
-    $scope.cities.push(cityName);
-    console.log($scope.cities);
-    
-  };
- 
-   $scope.showCityDetailsDialog = function(dialogId){
-    LxDialogService.open(dialogId);
-  };
-  $scope.addToVenue = function(venue){
-    $scope.venues.push(venue);
-    venue = '';
-  };
-
-
-
-
-
-
-
 }]);
 
 
